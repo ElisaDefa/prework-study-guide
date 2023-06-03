56111c973f9382a7c57cdba4fb997f2af4208869
Eli <elisadesfassiaux@gmail.com>
2023-06-03 14:06:22 -0600

    Added functions to JS file

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,30 @@
 //Declaring arrey
-
 var topics = ['HTML', 'CSS','Git','JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-//addiing for loop
+//Adding function & For Loop
 
-for(var x = 0; x < topics.length; x++)
-{
-    console.log(topics[x]);
+function listTopics(){
+    for(var x = 0; x < topics.length; x++)
+    {
+        console.log(topics[x]);
+    }
 }
+console.log("Here's the list of topics we learned in the Prework");
+listTopics();
 
-/*if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("LEt's study CSS");
-} else if (topic === 'Git') {
-    console.log("Let's study Git");
-} else if (topic === 'JavaScript') {
-    console.log("Let's Study JavaScript");
-} else {
-    console.log('Please try again!');
-}*/
+function selectTopic(){
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("LEt's study CSS");
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's Study JavaScript");
+    } else {
+        console.log('Please try again!')
+    }
+}
+console.log("What toppic should we study first?");
+selectTopic();
